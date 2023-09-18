ee17ab5077a1b1800d758dd0b992118edf1044cd
Sterling Jeppson <sterlingijeppson@gmail.com>
2023-09-17 18:32:35 -0700

    did work on fib.v

--- Fibonacci/Fib.v
+++ Fibonacci/Fib.v
@@ -1,21 +1,87 @@
-Require Import ZArith.
+Require Import ZArith Lia QArith.
 Open Scope Z_scope.
 
-Fixpoint fib (n : nat) : Z :=
+Fixpoint fibonacci (n : nat) : Z :=
   match n with
   | O => 1
   | S n' => match n' with
             | O => 1
-            | S n'' => fib(n') + fib(n'')
+            | S n'' => fibonacci(n') + fibonacci(n'')
             end
   end.
 
-Check fib.
-About fib.
+Global Notation F := fibonacci.
 
-Compute fib(1) - fib(5).
-Compute fib(3).
-Compute fib(5).
+Lemma fib_suc_suc : forall n : nat, F (S (S n)) = F (S n) + F n.
+Proof.
+  intros n.
+  destruct n as [| n'] eqn:En.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Lemma fib_suc_suc' : forall n : nat, F(n+2) = F(n+1) + F(n).
+Proof.
+  intro n. 
+  assert(H1: S n = (n+1)%nat).
+    { lia. } rewrite <- H1.
+  assert(H2: S (S n) = (n+2)%nat).
+    { lia. } rewrite <- H2.
+  apply fib_suc_suc.
+Qed.
+
+Lemma fib_suc_suc'' : forall n : nat, (n > 0)%nat -> F(n+1) = F(n) + F(n-1).
+Proof.
+  intros n H1. destruct n as [| n'] eqn:En.
+  - simpl. exfalso. apply (gt_irrefl) in H1. apply H1.
+  - assert (H2: S n' = (n' + 1)%nat).
+    { lia. } rewrite -> H2. rewrite <- Nat.add_assoc. simpl.
+    assert (H3: (n' + 1 - 1)%nat = n').
+    { lia. } rewrite -> H3.
+    apply fib_suc_suc'.
+Qed.
+
+Lemma fibonacci_positive: forall n : nat, fibonacci n > 0.
+Proof.
+  intros n.
+  induction n as [| n' IHn'].
+  - simpl. omega.
+  - destruct n' as [| n''].
+    + simpl. omega.
+    + simpl. 
+      assert (fibonacci n' > 0) by (apply IHn').
+      assert (fibonacci (S n'') > 0) by (apply IHn').
+      omega.
+
+Lemma next_fib_greater : forall n : nat,
+  fibonacci (S n) >= fibonacci n.
+Proof.
+  intros n. induction n as [| k IH].
+  - simpl. lia.
+  - induction k as [| k' IH'].
+    + simpl. lia.
+    + rewrite <- fib_suc_suc'. lia.
+Qed.
+
+
+
+Lemma fibonacci_positive: forall n : nat, fibonacci n > 0.
+Proof.
+  intros n.
+  induction n as [| n' IHn'].
+  - simpl. lia.
+  - destruct n' as [| n''].
+    + simpl. lia.
+    + rewrite <- Z.add_0_r at 1.
+      apply Z.add_lt_mono; assumption.
+Qed.
+
+Check F.
+About F.
+
+Compute F(1) - F(5).
+Compute F(3).
+Compute F(5).
 
 
 About Z.
@@ -29,24 +95,6 @@ Proof.
   rewrite -> Z.mul_1_r. reflexivity.
 Qed.
 
-Lemma fib_suc_suc : forall n : nat, fib (S (S n)) = fib (S n) + fib n.
-Proof.
-  intros n.
-  destruct n as [| n'] eqn:En.
-  - simpl. reflexivity.
-  - simpl. reflexivity.
-Qed.
-
-Lemma fib_suc_suc' : forall n : nat, fib(n+2) = fib(n+1) + fib(n).
-Proof.
-  intro n. 
-  assert(H1: S n = (n+1)%nat).
-    { lia. } rewrite <- H1.
-  assert(H2: S (S n) = (n+2)%nat).
-    { lia. } rewrite <- H2.
-  apply fib_suc_suc.
-Qed.
-
 Lemma pow_add_one_nat : forall k : nat, (-1)^(Z.of_nat (k + 1)) = (-1)^(Z.of_nat k) * (-1)^1.
 Proof.
   intros k.
@@ -58,10 +106,9 @@ Proof.
 Qed.
 
 
-Theorem fib_diff : forall (n : nat),
-  fib(n+2) * fib(n) - (fib(n+1) * fib(n+1)) = (Z.pow (-1) (Z.of_nat n)).
+Theorem F_diff : forall (n : nat),
+  F(n+2) * F(n) - (F(n+1) * F(n+1)) = (Z.pow (-1) (Z.of_nat n)).
 Proof.
-  Local Notation F := fib.
   intro n. induction n as [| k IH].
   - simpl. reflexivity.
   - replace (F (S k + 2)) with (F (k+2) + F(k + 1)).
@@ -94,27 +141,27 @@ Qed.
 
 
 Lemma lemma1 : forall n : nat,
-  fib(n+2) * fib(n) - fib(n+1) ^ 2 = (-1) ^ (Z.of_nat n).
+  F(n+2) * F(n) - F(n+1) ^ 2 = (-1) ^ (Z.of_nat n).
 Proof.
-  intro n. rewrite <- pow_2_z. apply fib_diff.
+  intro n. rewrite <- pow_2_z. apply F_diff.
 Qed.
 
-Example fib_test : fib (3602) * fib(3600) - fib(3601)^2 = (-1)^(3600).
+Example F_test : F (3602) * F(3600) - F(3601)^2 = (-1)^(3600).
 Proof. apply (lemma1 3600). Qed.
 
-Compute fib(15).
-Compute fib(20).
-Compute fib(25).
+Compute F(15).
+Compute F(20).
+Compute F(25).
 
 Require Import QArith.
 Require Import Peano.
 
-Compute ((inject_Z (fib 10)) / (inject_Z (fib 9)))%Q.
+Compute ((inject_Z (F 10)) / (inject_Z (F 9)))%Q.
 
 Definition a (n :nat) : Q :=
   match n with
   | O => 3
-  | _ => (inject_Z (fib(2 * n))) / (inject_Z (fib(2 * n - 1)))
+  | _ => (inject_Z (F(2 * n))) / (inject_Z (F(2 * n - 1)))
   end.
 
 Compute a(0).
@@ -123,7 +170,7 @@ Compute a(2).
 Compute a(3).
 Compute a(4).
 
-Definition b (n : nat) : Q := (inject_Z (fib(2*n + 1))) / (inject_Z (fib(2*n))).
+Definition b (n : nat) : Q := (inject_Z (F(2*n + 1))) / (inject_Z (F(2*n))).
 
 Compute b(0).
 Compute b(1).
@@ -137,65 +184,112 @@ Lemma lemmma : forall (n : nat), (n > 0)%nat -> (2 * n + 1)%nat = (2 * n - 1 + 2
 Proof.
   intros n H. induction n as [| k IH].
   - simpl. exfalso. apply (gt_irrefl) in H. apply H.
-  - lia.
+  - lia. 
 Qed.
 
 Search (_ - 0).
 
 Require Import ZArith.
 
+Check Z.pow_1_l.
 
-Lemma example : forall k : nat, (-1) ^ Z.of_nat (k * k + 1 + (2 * k + 1)) = -1.
-Proof.
-  intros k.
-  rewrite <- Z.pow_add_r.
-  rewrite Z.pow_1_r by omega.
-  rewrite Z.pow_add_r by (apply Nat2Z.is_nonneg; omega).
-  rewrite Z.pow_1_r by omega.
-  simpl.
-  reflexivity.
-Qed.
-
-
-Lemma minus_one_pow_odd : forall n : nat, (-1) ^ Z.of_nat (n * n + 1) = -1.
+Lemma minus_one_pow_odd : forall n : nat,
+  (-1) ^ Z.of_nat (2 * n + 1) < 0.
 Proof.
   intros n. induction n as [| k IH].
-  - simpl. reflexivity.
-  - assert (H: (S k * S k + 1)%nat = ((k * k + 1) + (2 * k + 1))%nat).
-    { lia. } rewrite -> H. rewrite  (Z.pow_add_r (-1) (Z.of_nat k * Z.of_nat k + 1) (2 * Z.of_nat k + 1)).
+  - simpl. lia.
+  - rewrite Nat2Z.inj_add. rewrite -> Z.pow_add_r.
+    -- assert (H: (-1) ^ Z.of_nat 1 = -1). { lia. } 
+       rewrite -> H. assert (H2 : (2 * S k = (2 * k + 1) + 1)%nat).
+       { lia. } rewrite -> H2. rewrite Nat2Z.inj_add. rewrite -> Z.pow_add_r.
+       --- rewrite -> H. rewrite <- Z.mul_assoc. assert (H3: -1 * -1 = 1).
+           { lia. } rewrite -> H3. rewrite Z.mul_1_r. apply IH.
+       --- lia.
+       --- lia.
+    -- lia.
+    -- lia.
 Qed.
 
-Lemma minus_one_pow_odd : forall n : nat, (-1) ^ Z.of_nat (n * n + 1) = -1.
+Lemma pow_n_squared_plus_1_unknown_sign : exists n m : nat,
+  (-1) ^ Z.of_nat (n * n + 1) = -1 /\ (-1) ^ Z.of_nat (m * m + 1) = 1.
 Proof.
-  intro n.
-  assert (H_odd: Z.odd (Z.of_nat (n * n + 1)) = true).
-  {
-    rewrite Nat2Z.inj_add.
-    rewrite Nat2Z.inj_mul.
-    apply Z.odd_spec.
-    exists (Z.of_nat n * Z.of_nat n).
-    rewrite -> Z.double.
-    reflexivity.
-  }
-
-  apply Z.pow_1_l.
-  rewrite Z.odd_spec in H_odd.
-  destruct H_odd as [k Hk].
-  rewrite Hk.
-  apply Z.add_1_r.
+  exists 2%nat. exists 1%nat. split.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
 Qed.
 
 
 Lemma lemma2 : forall (n : nat), 
-  (n > 0)%nat -> fib(2*n+1)*fib(2*n-1) - fib(2*n)^2 < 0.
+  (n > 0)%nat -> F(2*n+1)*F(2*n-1) - F(2*n)^2 < 0.
 Proof.
   intros n H. rewrite -> lemmma. replace (F (2 * n)) with (F (2 * n - 1 + 1)).
   rewrite -> lemma1. induction n.
   - simpl. exfalso. apply (gt_irrefl) in H. apply H.
   - simpl. rewrite plus_0_r. assert (H2: (n + S n - 0)%nat = (2 * n + 1)%nat).
-    { lia. } rewrite -> H2. 
+    { lia. } rewrite -> H2. apply minus_one_pow_odd. 
+  - assert (H2 : (2 * n - 1 + 1 = 2 * n)%nat). { lia. } rewrite -> H2. reflexivity.
+  - apply H.
+Qed.
 
-Abort.
+Lemma lemma3 : forall (n : nat),
+  (n > 0)%nat -> F(2*n+1)*F(2*n-1) < F(2*n)^2.
+Proof.
+  intros n H. apply lemma2 in H. lia.
+Qed.
+
+Lemma lemma4 : forall (n : nat),
+  (n > 0)%nat -> F (2*n) * F(2*n-1) + F(2*n+1)*F(2*n-1) < F(2*n)^2 + F(2*n) * F(2*n-1).
+Proof.
+  intros n H. apply lemma3 in H. lia.
+Qed.
+
+Lemma lemma5 : forall (n : nat),
+  (n > 0)%nat -> F(2*n-1) * (F (2*n+1) + F(2*n)) < F (2*n) * (F (2*n) + F (2*n-1)).
+Proof.
+  intros n H. apply lemma4 in H. lia.
+Qed.
+
+Lemma lemma6 : forall (n : nat),
+  (n > 0)%nat -> (F(2*n-1) * F(2*n+2)) < (F(2*n) * F(2*n+1)).
+Proof.
+  intros n H. 
+  replace (F (2 * n + 2)) with (F (2 * n + 1) + F (2 * n)).
+  replace (F (2 * n + 1)) with (F (2 * n) + F (2 * n - 1)) at 2.
+  - apply lemma5 in H. lia.
+  - apply symmetry. apply fib_suc_suc'' with (n := (2 * n)%nat). lia.
+  - apply symmetry. apply fib_suc_suc' with (n := (2 * n)%nat).
+Qed.
+
+Open Scope Q_scope.
+
+Lemma inject_Z_mul : forall a b : Z, inject_Z (a * b) == inject_Z a * inject_Z b.
+Proof.
+  intros.
+  unfold Qmult, inject_Z. simpl.
+  reflexivity.
+Qed.
+
+Lemma inject_Z_lt : forall a b : Z,
+  (a < b)%Z -> inject_Z a < inject_Z b.
+Proof.
+  intros a b. unfold inject_Z. unfold Qlt. simpl. 
+  repeat rewrite Z.mul_1_r. auto.
+Qed.
+
+
+Lemma lemma7 : forall (n : nat),
+  (n > 0)%nat -> 
+  inject_Z (F ((2 * n + 2))) / inject_Z (F (2 * n + 1)) < 
+  inject_Z (F (2 * n)) / inject_Z (F (2 * n - 1)).
+Proof.
+  intros n H1. apply lemma6 in H1. 
+  assert (H2 : inject_Z (F (2 * n - 1)) * inject_Z (F (2 * n + 2)) <
+             inject_Z (F (2 * n)) * inject_Z (F (2 * n + 1))).
+         { repeat rewrite <- inject_Z_mul. apply inject_Z_lt. apply H1. }
+  
+  
+  
+
 
 
 Lemma a_decreasing_bounded_below : forall (n : nat),
