c964a1fd958be5ce23917b00245bc1ae6fad9feb
Sterling Jeppson <sterlingijeppson@gmail.com>
2023-12-05 23:19:47 -0800

    finished problem 25

--- Spivak/Chapter1.v
+++ Spivak/Chapter1.v
@@ -534,7 +534,48 @@ Qed.
 Lemma lemma_1_25_P2 : forall a : number, 
   exists i : number, a + i = a.
 Proof.
+  intros a. exists zero. destruct a; reflexivity.
+Qed.
+
+Lemma lemma_1_25_P3 : forall a : number,
+  exists i : number, a + i = zero.
+Proof.
   intros a. destruct a.
+  - exists zero. reflexivity.
+  - exists one. reflexivity.
+Qed.
+
+Lemma lemma_1_25_P4 : forall a b : number,
+  a + b = b + a.
+Proof.
+  intros a b. destruct a, b; reflexivity.
+Qed.
+
+Lemma lemma_1_25_P6 : forall a : number,
+  exists i : number, a * i = a.
+Proof.
+  intros a. exists one. destruct a; reflexivity.
+Qed.
+
+Lemma lemma_1_25_P7 : forall a : number,
+  exists i : number, a * i = zero.
+Proof.
+  intros a. destruct a.
+  - exists one. reflexivity.
+  - exists zero. reflexivity.
+Qed.
+
+Lemma lemma_1_25_P8 : forall a b : number,
+  a * b = b * a.
+Proof.
+  intros a b. destruct a, b; reflexivity.
+Qed.
+
+Lemma lemma_1_25_P9 : forall a b c : number,
+  a * (b + c) = a * b + a * c.
+Proof.
+  destruct a, b, c; reflexivity.
+Qed.
 
 Theorem sum_n_nat : forall n : nat,
   sum_f 0 n (fun i => INR i) = (INR n * (INR n + 1)) / 2.
