ba639c4607cd2ebe4fc2fd7e0f604ebd3b742ac3
Sterling Jeppson <sterlingijeppson@gmail.com>
2023-12-30 13:47:31 -0800

    completed problem 17

--- Spivak/Chapter1.v
+++ Spivak/Chapter1.v
@@ -1153,7 +1153,6 @@ Proof.
       rewrite pow2_sqrt. lra. lra.
      }
     } 
-     
   } solve_abs. 
 Qed.
 
@@ -1406,7 +1405,39 @@ Proof.
                 rewrite H in H5. apply Rlt_irrefl in H5. exfalso. apply H5.
   - nra.
 Qed.
-  
+
+Lemma lemma_1_17_a' : forall x,
+  (x = 3 / 4 -> 2 * x^2 - 3 * x + 4 = 23 / 8) /\ 23 / 8 <= 2 * x^2 - 3 * x + 4.
+Proof.
+  intros x. split.
+  - intro H1. rewrite H1. nra.
+  - replace (2 * x^2 - 3 * x + 4) with (2 * (x - 3 / 4)^2 + 23 / 8) by nra.
+    assert (H1 : (x - 3 / 4)^2 >= 0). { apply Rle_ge. rewrite <- Rsqr_pow2. apply Rle_0_sqr. }
+    nra.
+Qed.
+
+Lemma lemma_1_17_b : forall x y,
+  (x = 3 / 2 /\ y = -1 -> x^2 - 3 * x + 2 * y^2 + 4 * y + 2 = -9 / 4) /\ -9 / 4 <= x^2 - 3 * x + 2 * y^2 + 4 * y + 2.
+Proof.
+  intros x y. split.
+  - intros [H1 H2]. rewrite H1, H2. nra.
+  - replace (x^2 - 3 * x + 2 * y^2 + 4 * y + 2) with ((x - 3 / 2)^2 + 2 * (y + 1)^2 - 9 / 4) by nra.
+    assert (H1 : (x - 3 / 2)^2 >= 0). { apply Rle_ge. rewrite <- Rsqr_pow2. apply Rle_0_sqr. }
+    assert (H2 : (y + 1)^2 >= 0). { apply Rle_ge. rewrite <- Rsqr_pow2. apply Rle_0_sqr. }
+    nra.
+Qed.
+
+Lemma lemma_1_17_c : forall x y,
+  ((x = 4 /\ y = -1) -> x^2 + 4 * x * y + 5 * y^2 - 4 * x - 6 * y + 7 = 2) /\ 2 <= x^2 + 4 * x * y + 5 * y^2 - 4 * x - 6 * y + 7.
+Proof.
+  intros x y. split.
+  - intros [H1 H2]. rewrite H1, H2. nra.
+  - replace (x^2 + 4 * x * y + 5 * y^2 - 4 * x - 6 * y + 7) with ((x + 2 * y - 2)^2 + (y + 1)^2 + 2) by nra.
+    assert (H1 : (x + 2 * y - 2)^2 >= 0). { apply Rle_ge. rewrite <- Rsqr_pow2. apply Rle_0_sqr. }
+    assert (H2 : (y + 1)^2 >= 0). { apply Rle_ge. rewrite <- Rsqr_pow2. apply Rle_0_sqr. }
+    nra.
+Qed. 
+
 Lemma lemma_1_20 : forall x x0 y y0 eps,
   Rabs (x - x0) < eps / 2 -> Rabs (y - y0) < eps / 2 -> (Rabs ((x + y) - (x0 + y0)) < eps /\ Rabs ((x - y) - (x0 - y0)) < eps).
 Proof.
