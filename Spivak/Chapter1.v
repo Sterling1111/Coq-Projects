6990f435f7bf2f7e878b4702d3c53883962bbd7b
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-01-01 01:22:26 -0800

    completed problem 22

--- Spivak/Chapter1.v
+++ Spivak/Chapter1.v
@@ -1568,6 +1568,44 @@ Proof.
   nra.
 Qed.
 
+Lemma lemma_1_22 : forall y y0 eps,
+  (y0 <> 0) -> (Rabs (y - y0) < Rmin (Rabs (y0 / 2)) ((eps * (Rabs y0)^2) / 2)) -> (y <> 0 /\ Rabs (1 / y - 1 / y0) < eps).
+Proof.
+  intros y y0 eps H1 H2. assert (H3 : y <> 0).
+  - assert (H4 : Rabs (y - y0) < Rabs (y0 / 2)). { apply Rlt_gt in H2. apply Rmin_Rgt_l in H2. lra. } solve_abs.
+  - split.
+    -- apply H3.
+    -- assert (H4 : Rabs (y - y0) < Rabs (y0 / 2)). { apply Rlt_gt in H2. apply Rmin_Rgt_l in H2. lra. }
+       assert (H5 : Rabs (y - y0) < (eps * (Rabs y0)^2) / 2). { apply Rlt_gt in H2. apply Rmin_Rgt_l in H2. lra. }
+       assert (H6 : Rabs y > Rabs y0 / 2) by solve_abs.
+       assert (H7 : Rabs y > 0) by solve_abs. assert (H8 : Rabs y0 > 0) by solve_abs.
+       assert (H9 : forall a b : R, a > 0 -> b > 0 -> a > b / 2 -> 1 / a < 2 / b).
+       { 
+          intros a b H9 H10 H11. apply Rmult_lt_reg_r with (r := a). lra. replace (1 / a * a) with 1 by (field; lra).
+          apply Rmult_lt_reg_r with (r := b). lra. replace (2 / b * a * b) with (2 * a) by (field; lra). lra.
+       }
+       assert (H10 : 1 / Rabs y < 2 / Rabs y0). { apply H9. apply H7. apply H8. apply H6. } clear H9.
+       replace (1 / y - 1 / y0) with ((y0 - y) / (y * y0)) by (field; lra). 
+       unfold Rdiv. rewrite Rabs_mult. rewrite Rabs_inv. rewrite <- Rdiv_def. rewrite Rabs_mult.
+       rewrite Rabs_minus_sym. assert (H11 : 2 * Rabs (y - y0) < eps * Rabs y0 ^ 2). { lra. }
+       assert (H12 : (2 * Rabs (y - y0)) / (Rabs y0 ^ 2) < eps).
+       { apply Rmult_lt_reg_r with (r := Rabs y0 ^ 2). nra. apply Rmult_lt_reg_r with (r := / 2). nra.
+          replace (2 * Rabs (y - y0) / (Rabs y0 ^ 2) * Rabs y0 ^2 * / 2) with 
+          (2 * Rabs (y - y0) * / 2) by (field; lra). lra.
+       }
+       replace (2 * Rabs (y - y0) / Rabs y0 ^ 2) with (Rabs (y - y0) / ((Rabs y0 / 2) * Rabs y0)) in H12 by (field; nra).
+       assert (H13 : (Rabs y0 / 2 * Rabs y0) < Rabs y * Rabs y0) by nra. 
+       assert (H14 : forall a b c, a > 0 -> b > 0 -> c >= 0 -> a > b -> c / a <= c / b).
+       {
+          intros a b c H14 H15 H16 H17. apply Rmult_le_reg_r with (r := a). lra.
+          replace (c / a * a) with c by (field; lra). apply Rmult_le_reg_r with (r := b). lra.
+          replace (c / b * a * b) with (c * a) by (field; lra). nra.
+       }
+       assert (H15 : Rabs (y - y0) / (Rabs y0 / 2 * Rabs y0) >= Rabs (y - y0) / (Rabs y * Rabs y0)). 
+       { apply Rle_ge. apply H14. nra. nra. apply Rle_ge. apply Rabs_pos. nra. }
+       nra.
+Qed. 
+
 Fixpoint fold_right' (l: list R) : R :=
   match l with
   | [] => 0
