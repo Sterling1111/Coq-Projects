d0c5c708258ff00a8aa515b49c97a094d0f6e2f4
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-02-04 07:26:23 -0800

    almost finished problem 13

--- Spivak/Chapter2.v
+++ Spivak/Chapter2.v
@@ -809,6 +809,68 @@ Proof.
   { lia. } { tauto. } { tauto. }
 Qed.
 
+Lemma kcub_even_k_even : forall k,
+  Z.Even (k^3) -> Z.Even k.
+Proof.
+  intros k H1. pose proof lemma_2_8' k as [H2 | H2].
+  - auto.
+  - destruct H2 as [k' H2]. assert (Z.Odd (k^3)) as H3.
+    { exists (4*k'^3 + 6*k'^2 + 3*k'). nia. } rewrite <- Zodd_equiv in H3.
+    rewrite <- Zeven_equiv in H1. apply Zodd_not_Zeven in H3. tauto.
+Qed.
+
+Lemma lemma_2_13_b : irrational (Rpower 2 (1/3)).
+Proof.
+  unfold irrational. unfold rational. unfold not. intros [z1 [z2 H1]].
+  assert (H2 : Rpower 2 (1/3) <> 0%R). 
+  { assert (H3 : (Rpower 2 0 < Rpower 2 (1/3))%R) by (apply Rpower_lt; lra). rewrite Rpower_O in H3; lra. }
+  assert (z1 <> 0 /\ z2 <> 0) as [H3 H4].
+  { assert (z1 = 0 \/ z2 = 0 \/ z1 <> 0 /\ z2 <> 0) as [H3 | [H3 | H3]] by lia; auto; rewrite H3 in H1; try rewrite Rdiv_0_l in H1; try rewrite Rdiv_0_r in H1; lra. }
+  assert (H5 : exists z1' z2', ((Rpower 2 (1/3) = IZR z1' / IZR z2')%R /\ (forall x, x > 1 -> ~(x | z1') \/ ~(x | z2')))).
+  { apply rational_representation with (z1 := z1) (z2 := z2). apply H3. apply H4. apply H1. }
+  destruct H5 as [z1' [z2' [H5 H6]]]. assert (H7 : (Rpower 2 (1/3) * Rpower 2 (1/3) * Rpower 2 (1/3) = (IZR z1' / IZR z2') * (IZR z1' / IZR z2') * (IZR z1' / IZR z2'))%R) by nra.
+  assert (H8 : z1' <> 0 /\ z2' <> 0).
+  { assert (z1' = 0 \/ z2' = 0 \/ z1' <> 0 /\ z2' <> 0) as [H8 | [H8 | H8]] by lia; auto; rewrite H8 in H5; try rewrite Rdiv_0_l in H5; try rewrite Rdiv_0_r in H5; lra. }
+  replace (Rpower 2 (1/3) * Rpower 2 (1/3) * Rpower 2 (1/3))%R with 2%R in H7.
+  2 : { repeat rewrite <- Rpower_plus. replace (1/3 + 1/3 + 1/3)%R with 1%R by lra. rewrite Rpower_1;lra. }
+  replace (IZR z1' / IZR z2' * (IZR z1' / IZR z2') * (IZR z1' / IZR z2'))%R with ((IZR z1')^3 / (IZR z2')^3)%R in H7 by (field; apply not_0_IZR; tauto).
+    apply Rmult_eq_compat_r with (r := ((IZR z2')^3)%R) in H7. replace ((IZR z1' ^ 3 / IZR z2' ^ 3 * IZR z2' ^ 3)%R) with ((IZR z1')^3)%R in H7 by (field; apply not_0_IZR; tauto).
+  replace 3 % nat with (Z.to_nat 3) in H7 at 1 by auto.
+  assert (Z.Even (z1'^3)) as H9. 
+  { replace 3 with (Z.of_nat 3) by auto. exists (z2'^3). apply eq_IZR. rewrite <- pow_IZR. rewrite mult_IZR. 
+    apply eq_sym. rewrite pow_IZR in H7. replace (Z.of_nat (Z.to_nat 3)) with (3) in H7 by lia. apply H7. }
+  assert (H10 : Z.Even z1'). { apply kcub_even_k_even in H9. auto. }
+  assert (H11 : (2 | z1')). { destruct H10 as [k H10]. exists (k). lia. }
+  destruct H10 as [k H10]. rewrite H10 in H7. assert (H12 : (IZR z2' ^ 3 = 4 * IZR (k^3))%R).
+  { replace 3%nat with (Z.to_nat 3) in H7 by auto. rewrite pow_IZR with (z := 2 * k) in H7. replace (Z.to_nat 3) with 3%nat in H7 by auto.
+    replace (Z.of_nat 3) with 3 in H7 by auto. replace ((2 * k) ^ 3) with (8 * k^3) in H7 by lia. rewrite mult_IZR in H7. nra. }
+  assert (Z.Even (z2'^3)) as H13. 
+  { replace 3 with (Z.of_nat 3) by auto. exists (2 * k^3). apply eq_IZR. rewrite <- pow_IZR. rewrite mult_IZR. 
+    rewrite mult_IZR. replace (2 * (2 * IZR (k^3)))%R with (4 * IZR (k^3))%R by lra. nra. }
+  assert (H14 : Z.Even z2'). { apply kcub_even_k_even in H13. auto. }
+  assert (H15 : (2 | z2')). { destruct H14 as [k' H14]. exists (k'). lia. }
+  specialize (H6 2) as [H6 | H6]; try lia; tauto.
+Qed.
+
+Lemma lemma_2_13_b' : irrational (Rpower 3 (1/3)).
+Proof.
+  unfold irrational. unfold rational. unfold not. intros [z1 [z2 H1]].
+  assert (H2 : Rpower 3 (1/3) <> 0%R). 
+  { assert (H3 : (Rpower 3 0 < Rpower 3 (1/3))%R) by (apply Rpower_lt; lra). rewrite Rpower_O in H3; lra. }
+  assert (z1 <> 0 /\ z2 <> 0) as [H3 H4].
+  { assert (z1 = 0 \/ z2 = 0 \/ z1 <> 0 /\ z2 <> 0) as [H3 | [H3 | H3]] by lia; auto; rewrite H3 in H1; try rewrite Rdiv_0_l in H1; try rewrite Rdiv_0_r in H1; lra. }
+  assert (H5 : exists z1' z2', ((Rpower 3 (1/3) = IZR z1' / IZR z2')%R /\ (forall x, x > 1 -> ~(x | z1') \/ ~(x | z2')))).
+  { apply rational_representation with (z1 := z1) (z2 := z2). apply H3. apply H4. apply H1. }
+  destruct H5 as [z1' [z2' [H5 H6]]]. assert (H7 : (Rpower 3 (1/3) * Rpower 3 (1/3) * Rpower 3 (1/3) = (IZR z1' / IZR z2') * (IZR z1' / IZR z2') * (IZR z1' / IZR z2'))%R) by nra.
+  assert (H8 : z1' <> 0 /\ z2' <> 0).
+  { assert (z1' = 0 \/ z2' = 0 \/ z1' <> 0 /\ z2' <> 0) as [H8 | [H8 | H8]] by lia; auto; rewrite H8 in H5; try rewrite Rdiv_0_l in H5; try rewrite Rdiv_0_r in H5; lra. }
+  replace (Rpower 3 (1/ 3) * Rpower 3 (1/3) * Rpower 3 (1/3))%R with 3%R in H7. 
+  2 : { repeat rewrite <- Rpower_plus. replace (1/3 + 1/3 + 1/3)%R with 1%R by lra. rewrite Rpower_1;lra. }
+  replace (IZR z1' / IZR z2' * (IZR z1' / IZR z2') * (IZR z1' / IZR z2'))%R with ((IZR z1')^3 / (IZR z2')^3)%R in H7 by (field; apply not_0_IZR; tauto).
+    apply Rmult_eq_compat_r with (r := ((IZR z2')^3)%R) in H7. replace ((IZR z1' ^ 3 / IZR z2' ^ 3 * IZR z2' ^ 3)%R) with ((IZR z1')^3)%R in H7 by (field; apply not_0_IZR; tauto).
+  replace 3 % nat with (Z.to_nat 3) in H7 at 1 by auto.
+Abort.
+
 Fixpoint max_list_Z (l : list Z) : Z :=
   match (l) with
   | [] => 0
