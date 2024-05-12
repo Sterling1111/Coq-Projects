a3d555588a6c1c1387c50a866f36f643c8b0dbc6
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-05-11 18:57:54 -0700

    completed problem 5_b

--- Spivak/Chapter2.v
+++ Spivak/Chapter2.v
@@ -267,13 +267,24 @@ Proof.
   { unfold Sum. rewrite r_mult_sum_f_i_n_f. apply sum_f_equiv. lia. intros k H2. simpl. lra. }
   assert (r = 0 \/ r <> 0) as [H3 | H3] by lra; assert (n = 0 \/ n > 0)%nat as [H4 | H4] by lia.
   - unfold Sum. rewrite H4. rewrite H3. rewrite sum_f_0_0. simpl. field.
-  - unfold Sum. rewrite H3 in *. replace (sum_f 0 n (fun i => 0 ^ i)) with (sum_f 0 n (fun i => 0)).
-    2 : { apply sum_f_equiv. lia. intros k H5. destruct k. simpl. lra. simpl. lra. }
-    replace (sum_f 0 n (fun i => 0)) with 0 by (apply sum_f_0_0). lra.
-  unfold Sum. apply Rmult_eq_reg_l with (r := r).
-Abort.
-
-Open Scope nat_scope.
+  - unfold Sum. rewrite H3 in *. rewrite sum_f_Si; try lia. 
+    replace (sum_f 1 n (fun i => 0 ^ i)) with (sum_f 1 n (fun i => 0)).
+    2 : { apply sum_f_equiv. lia. intros k H5. destruct k. lia. simpl. lra. }
+    rewrite sum_f_const. simpl. replace (n+1)%nat with (S n) by lia. simpl. lra.
+  - unfold Sum. rewrite H4 in *. rewrite sum_f_0_0. simpl. field; lra.
+  -  assert (H5 : r * Sum = sum_f 1 (n+1) (fun i : nat => r^i)).
+    { rewrite sum_f_reindex with (s := 1%nat); try lia. simpl. replace (n + 1 - 1)%nat with n%nat by lia.
+      replace (fun x : nat => r ^ (x + 1)) with (fun x : nat => r ^ (S x)). 
+      2 : { apply functional_extensionality. intros x. replace (x + 1)%nat with (S x) by lia. reflexivity. }
+      rewrite H2. apply sum_f_equiv; try lia. intros k H5. reflexivity.
+    } assert (H6 : Sum - r * Sum = 1 - r^(n + 1)).
+    { rewrite H5. unfold Sum. rewrite sum_f_Si_n_f with (i := 0%nat); try lia. replace (n+1)%nat with (S n) by lia.
+      rewrite sum_f_i_Sn_f with (n := n); try lia. lra. }
+    assert (H7 : Sum * (1 - r) = 1 - r ^ (n+1)) by nra. apply Rmult_eq_compat_r with (r := 1 / (1 - r)) in H7.
+    field_simplify in H7; try nra. rewrite H7. field; nra.
+Qed.
+  
+Open Scope nat_scope. 
 
 Lemma lemma_2_8 : forall n : nat,
   Nat.Even n \/ Nat.Odd n.
