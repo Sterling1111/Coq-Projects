8b8ce1a6fedf2bac108f255c97dfe8060d53536d
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-09-29 03:09:19 -0700

    finished lemma_14_4 in chapter 14

--- Seth/Chapter14.v
+++ Seth/Chapter14.v
@@ -1,4 +1,6 @@
-
+Require Import ZArith Lia Classical Reals Lra Classical_sets List Ensembles QArith ClassicalFacts Finite_sets.
+Import ListNotations.
+From Seth Require Export Chapter13 Sums.
 
 (* just do std lib Power_Set dont use this*)
 Definition Power_Set {U : Type} (A : Ensemble U) : Ensemble (Ensemble U) :=
@@ -12,3 +14,15 @@ Proposition prop_14_6 : forall (U : Type) (A : Ensemble U) (n : ℕ),
 Proof.
   intros U A n [m [l H1]] H2. 
 Admitted.
+
+Lemma lemma_14_4 : forall (l : list Prop),
+  ~ (fold_right and True l) -> fold_right or False (map (fun P => ~ P) l).
+Proof.
+  intros l H1. induction l as [| h t IH].
+  - simpl in H1. exfalso. apply H1. apply I.
+  - rewrite map_cons. replace (((~ h) :: map (fun P : Prop => ~ P) t)) with ([~ h] ++ map (fun P : Prop => ~ P) t) by reflexivity.
+    rewrite fold_right_app. simpl. replace (h :: t) with ([h] ++ t) in H1 by reflexivity. rewrite fold_right_app in H1. simpl in H1.
+    apply not_and_or in H1 as [H2 | H2].
+    -- left. auto.
+    -- right. apply (IH H2).
+Qed.
